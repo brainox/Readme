0af6b873ca3fe27ad1b47331f0733dfe6fd162a2
brainox <macmartins081@gmail.com>
2021-11-05 18:17:34 +0100

    added NewBooksViewController

---
+++ Readme/NewBooksViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewBooksViewController.swift
+//  Readme
+//
+//  Created by Decagon on 05/11/2021.
+//
+
+import UIKit
+
+class NewBooksViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
